cbfe03a4431dbf28f1de3e5ace188aa1711379c7
Hiroyuki IMAI <hiroyuki_imai_aa@mail.toyota.co.jp>
2022-11-02 15:09:57 +0900

    feat: add pokemon card style dom
    Co-authored-by: 1490563 <1490563@users.noreply.github.com>
    Co-authored-by: kosuke-ando <kosuke-ando@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -5,21 +5,81 @@ window.app = () => {
   pokeListDiv.id = "poke-list-div";
   main.append(pokeListDiv);
 
-  fetchPokeList();
+  fetchPokeNameListByGeneration(2).then((pokeNameList) => {
+    for (const pokeName of pokeNameList) {
+      fetchPoke(pokeName).then((poke) => renderPoke(poke));
+    }
+  });
 };
 
 const renderPoke = (poke) => {
   const pokeDiv = document.createElement("div");
   pokeDiv.className = "poke-div";
   pokeDiv.id = poke.name;
-  pokeDiv.innerText = poke.name;
+
+  const pokeDivUpper = document.createElement("div");
+  pokeDivUpper.className = "poke-div-upper";
+
+  const pokeGeneration = document.createElement("div");
+  pokeGeneration.className = "poke-generation";
+
+  const pokeNo = document.createElement("div");
+  pokeNo.className = "poke-no";
+
+  const pokeName = document.createElement("div");
+  pokeName.className = "poke-name";
+  pokeName.innerText = poke.name;
+
+  pokeDivUpper.append(pokeGeneration, pokeNo, pokeName);
+
+  const pokeDivMiddle = document.createElement("div");
+  pokeDivMiddle.className = "poke-div-middle";
+
+  const pokeImage = document.createElement("img");
+  pokeImage.className = "poke-image";
+
+  pokeDivMiddle.append(pokeImage);
+
+  const pokeDivBottom = document.createElement("div");
+  pokeDivBottom.className = "poke-div-bottom";
+
+  const pokeType = document.createElement("div");
+  pokeType.className = "poke-type";
+
+  const pokeHeight = document.createElement("div");
+  pokeHeight.className = "poke-height";
+
+  const pokeWeight = document.createElement("div");
+  pokeWeight.className = "poke-weight";
+
+  pokeDivBottom.append(pokeType, pokeHeight, pokeWeight);
+
+  pokeDiv.append(pokeDivUpper, pokeDivMiddle, pokeDivBottom);
+
   const pokeListDiv = document.getElementById("poke-list-div");
   pokeListDiv.append(pokeDiv);
 };
 
 const fetchPokeList = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon/")
+  return fetch("https://pokeapi.co/api/v2/pokemon/")
     .then((response) => response.json())
-    .then((data) => data.results.forEach((poke) => renderPoke(poke)))
+    .then((data) => data.results)
+    .catch((response) => response.json());
+};
+
+const fetchPokeNameList = () => {
+  return fetchPokeList().then((pokeList) => pokeList.map((elem) => elem.name));
+};
+
+const fetchPoke = (idOrName) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}/`)
+    .then((response) => response.json())
     .catch((response) => console.log(response));
+};
+
+const fetchPokeNameListByGeneration = (id) => {
+  return fetch(`https://pokeapi.co/api/v2/generation/${id}/`)
+    .then((response) => response.json())
+    .then((data) => data.pokemon_species.map((element) => element.name))
+    .catch((response) => response.json());
 };
