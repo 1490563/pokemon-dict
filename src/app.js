15a2d58af6a15b6100347a4549562dfe6be32c13
Hiroyuki IMAI <hiroyuki_imai_aa@mail.toyota.co.jp>
2022-11-02 17:42:28 +0900

    add images

--- src/app.js
+++ src/app.js
@@ -13,30 +13,42 @@ window.app = () => {
 };
 
 const renderPoke = (poke) => {
-  const pokeDiv = document.createElement("div");
-  pokeDiv.className = "poke-div";
-  pokeDiv.id = poke.name;
+  const pokeDivOuter = document.createElement("div");
+  pokeDivOuter.className = "poke-div-outer";
+  pokeDivOuter.id = poke.name;
+
+  const pokeDivInner = document.createElement("div");
+  pokeDivInner.className = "poke-div-inner";
 
   const pokeDivUpper = document.createElement("div");
   pokeDivUpper.className = "poke-div-upper";
 
+  const pokeHeader = document.createElement("div");
+  pokeHeader.className = "poke-header";
+
   const pokeGeneration = document.createElement("div");
   pokeGeneration.className = "poke-generation";
+  pokeGeneration.innerText = "###";
 
   const pokeNo = document.createElement("div");
   pokeNo.className = "poke-no";
+  pokeNo.innerText = poke.id;
+
+  pokeHeader.append(pokeGeneration, pokeNo);
 
   const pokeName = document.createElement("div");
   pokeName.className = "poke-name";
   pokeName.innerText = poke.name;
 
-  pokeDivUpper.append(pokeGeneration, pokeNo, pokeName);
+  pokeDivUpper.append(pokeHeader, pokeName);
 
   const pokeDivMiddle = document.createElement("div");
   pokeDivMiddle.className = "poke-div-middle";
 
   const pokeImage = document.createElement("img");
   pokeImage.className = "poke-image";
+
+  fetchPokeImage(poke.id, pokeImage);
 
   pokeDivMiddle.append(pokeImage);
 
@@ -46,18 +58,29 @@ const renderPoke = (poke) => {
   const pokeType = document.createElement("div");
   pokeType.className = "poke-type";
 
+  pokeType.innerText = poke.types.reduce((acc, curr, index) => {
+    if (index === 0) {
+      return curr.type.name;
+    } else {
+      return acc + ", " + curr.type.name;
+    }
+  }, "");
+
   const pokeHeight = document.createElement("div");
   pokeHeight.className = "poke-height";
+  pokeHeight.innerText = Number(poke.height) * 10 + " cm";
 
   const pokeWeight = document.createElement("div");
   pokeWeight.className = "poke-weight";
+  pokeWeight.innerText = Number(poke.weight) / 10 + " kg";
 
   pokeDivBottom.append(pokeType, pokeHeight, pokeWeight);
 
-  pokeDiv.append(pokeDivUpper, pokeDivMiddle, pokeDivBottom);
+  pokeDivInner.append(pokeDivUpper, pokeDivMiddle, pokeDivBottom);
+  pokeDivOuter.append(pokeDivInner);
 
   const pokeListDiv = document.getElementById("poke-list-div");
-  pokeListDiv.append(pokeDiv);
+  pokeListDiv.append(pokeDivOuter);
 };
 
 const fetchPokeList = () => {
@@ -82,4 +105,15 @@ const fetchPokeNameListByGeneration = (id) => {
     .then((response) => response.json())
     .then((data) => data.pokemon_species.map((element) => element.name))
     .catch((response) => response.json());
+};
+
+const fetchPokeImage = (id, pokeImage) => {
+  return fetch(
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+  )
+    .then((response) => response.blob())
+    .then((blobResponse) => {
+      const fileURL = URL.createObjectURL(blobResponse);
+      pokeImage.src = fileURL;
+    });
 };
